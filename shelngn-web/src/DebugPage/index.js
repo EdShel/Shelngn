21e0869d1eb9590719490dda6784cf80298e57b0
EdShel <eduardchelemetjev@gmail.com>
2022-05-11 08:29:09 +0300

    Move game running logic out of React folder

--- shelngn-web/src/DebugPage/index.js
+++ shelngn-web/src/DebugPage/index.js
@@ -1,182 +1,38 @@
 import React, { useEffect, useRef, useState } from "react";
-import { clearCanvas, initializeRenderer, renderBegin, unitializeRenderer } from "../core/rendering";
-import { begin, flush } from "../core/rendering/draw-batch";
-import Texture from "../core/rendering/texture";
-import Draw from "../core/rendering/Draw";
-import Camera2D from "../core/rendering/Camera2D";
-import GameScreen from "../core/rendering/GameScreen";
 import useWorkspaceId from "../WorkspacePage/hooks/useWorkspaceId";
-import { addFpsChangeListener, updateFps } from "../core/rendering/Fps";
+import { addFpsChangeListener } from "../core/rendering/Fps";
 import { getBuiltJsBundle, getBuiltResourceFile } from "../api";
 import styles from "./styles.module.css";
-import { deepEqual } from "./util";
 import { useShowAlertNotification } from "../InfoAlert";
+import { runner } from "../core/runner";
 
 const DebugPage = () => {
   const workspaceId = useWorkspaceId();
   const [fps, setFps] = useState(0);
-  const gameWebWorkerRef = useRef(null);
   const canvasRef = useRef();
   const { showError } = useShowAlertNotification();
 
   useEffect(() => {
-    let running = true;
-
-    const fetchGame = async () => {
-      if (!canvasRef.current) {
-        return;
-      }
-      initializeRenderer(canvasRef.current);
-      let loadedTextures = {};
-
-      const getTexture = (textureUrl) => {
-        if (loadedTextures[textureUrl]) {
-          return loadedTextures[textureUrl];
-        }
-        const textureAbsoluteUrl = getBuiltResourceFile(workspaceId, textureUrl);
-        return (loadedTextures[textureUrl] = Texture.loadLazy(textureAbsoluteUrl));
-      };
-
-      const proxyObjects = {
-        Draw: {
-          texture(textureUrl, x, y) {
-            const txt = getTexture(textureUrl);
-            Draw.texture(txt, x, y);
-          },
-          stretchedTexture(textureUrl, x, y, width, height, rotation, origin) {
-            const txt = getTexture(textureUrl);
-            Draw.stretchedTexture(txt, x, y, width, height, rotation, origin);
-          },
-        },
-      };
-
-      let state = {
-        screenDimensions: { width: 0, height: 0 },
-      };
-
-      const bundleSourceCode = await getBuiltJsBundle(workspaceId);
-      const bundleBlob = new Blob([bundleSourceCode]);
-      const scriptUrl = URL.createObjectURL(bundleBlob);
-      gameWebWorkerRef.current = new Worker(scriptUrl);
-      gameWebWorkerRef.current.onerror = e => {
-        showError(e.message);
-      }
-      gameWebWorkerRef.current.onmessage = ({ data }) => {
-        const camera = new Camera2D(
-          -state.screenDimensions.width / 2,
-          -state.screenDimensions.height / 2,
-          state.screenDimensions.width,
-          state.screenDimensions.height
-        );
-        Draw.applyCamera(camera);
-
-        renderBegin();
-        clearCanvas([0.2, 0.2, 0.2, 1]);
-        begin();
-
-        for (const drawCall of data) {
-          const [objectName, functionName, ...args] = drawCall;
-          proxyObjects[objectName][functionName](...args);
-        }
-        flush();
-        if (running) {
-          window.requestAnimationFrame(render);
-        }
-      };
-      URL.revokeObjectURL(scriptUrl);
-
-      const render = () => {
-        if (!gameWebWorkerRef.current) {
-          return;
-        }
-
-        updateFps();
-
-        let stateIncr = {};
-
-        const screenDimensions = GameScreen.getDimensions();
-        if (!deepEqual(state.screenDimensions, screenDimensions)) {
-          state.screenDimensions = screenDimensions;
-          stateIncr = { screenDimensions };
-        }
-
-        gameWebWorkerRef.current.postMessage({ type: "draw", stateIncr });
-      };
-      window.requestAnimationFrame(render);
-    };
-    fetchGame();
-
-    return () => {
-      gameWebWorkerRef.current?.terminate();
-      running = false;
-      unitializeRenderer();
-    };
-  }, []);
+    const loadBundle = () => getBuiltJsBundle(workspaceId);
+    const textureUriResolver = (url) => getBuiltResourceFile(workspaceId, url);
+    const onError = (e) => showError(e);
+    const gameInstance = runner(canvasRef.current, {
+      loadBundle,
+      textureUriResolver,
+      onError,
+    });
+    return gameInstance.cleanup;
+  }, [workspaceId]);
 
   useEffect(() => {
     const subscriber = addFpsChangeListener(setFps);
     return () => subscriber.remove();
   }, []);
 
-  // useEffect(() => {
-  //   if (!canvasRef.current) {
-  //     return;
-  //   }
-  //   initializeRenderer(canvasRef.current);
-
-  //   const texture = Texture.loadLazy("/logo192.png");
-
-  //   let running = true;
-  //   const render = () => {
-  //     updateFps();
-  //     const screenDimensions = GameScreen.getDimensions();
-
-  //     const camera = new Camera2D(
-  //       -screenDimensions.width / 2,
-  //       -screenDimensions.height / 2,
-  //       screenDimensions.width,
-  //       screenDimensions.height
-  //     );
-  //     camera.rotation = new Date().getTime() / 10;
-  //     camera.zoom = Math.sin(new Date().getTime() / 500);
-
-  //     Draw.applyCamera(camera);
-  //     renderBegin();
-  //     clearCanvas([0.2, 0.2, 0.2, 1]);
-  //     begin();
-
-  //     const x = Math.abs(-Math.sin(new Date().getTime() / 1000)) * 512;
-  //     Draw.drawTexture(texture, x - texture.width / 2, -texture.height / 2);
-  //     Draw.drawTexture(texture, -texture.width / 2, x - texture.height / 2);
-  //     Draw.drawTexture(texture, -texture.width / 2, -x - texture.height / 2);
-  //     Draw.drawTexture(texture, -x - texture.width / 2, -texture.height / 2);
-  //     flush();
-
-  //     if (running) {
-  //       window.requestAnimationFrame(render);
-  //     }
-  //   };
-  //   window.requestAnimationFrame(render);
-
-  //   return () => {
-  //     console.log("cleaning up canvas");
-  //     running = false;
-  //     unitializeRenderer();
-  //   };
-  // }, []);
-
   return (
     <div className={styles.container}>
-      <canvas
-        ref={canvasRef}
-        style={{
-          position: "absolute",
-          display: "block",
-          width: "100%",
-          height: "100%",
-        }}
-      />
-      <div style={{ position: "absolute", left: 10, top: 10 }}>{fps} FPS</div>
+      <canvas ref={canvasRef} className={styles.canvas} />
+      <div className={styles["fps-counter"]}>{fps} FPS</div>
     </div>
   );
 };
--- shelngn-web/src/core/runner/index.js
+++ shelngn-web/src/core/runner/index.js
@@ -1,95 +1,105 @@
-import { initializeRenderer } from "../rendering";
+import { clearCanvas, initializeRenderer, renderBegin, unitializeRenderer } from "../rendering";
+import Camera2D from "../rendering/Camera2D";
+import Draw from "../rendering/Draw";
+import { begin, flush } from "../rendering/draw-batch";
+import { updateFps } from "../rendering/Fps";
+import GameScreen from "../rendering/GameScreen";
+import Texture from "../rendering/texture";
+import { deepEqual } from "../util/deepEqual";
 
-export function runner(canvas, textureUriResolver) {
-    let running = true;
+export function runner(canvas, { loadBundle, textureUriResolver, onError }) {
+  let running = true;
+  let gameWebWorker = null;
 
-    const fetchGame = async () => {
-      if (!canvas) {
+  const fetchGame = async () => {
+    if (!canvas) {
+      return;
+    }
+    initializeRenderer(canvas);
+    let loadedTextures = {};
+
+    const getTexture = (textureUrl) => {
+      if (loadedTextures[textureUrl]) {
+        return loadedTextures[textureUrl];
+      }
+      const textureAbsoluteUrl = textureUriResolver(textureUrl);
+      return (loadedTextures[textureUrl] = Texture.loadLazy(textureAbsoluteUrl));
+    };
+
+    const proxyObjects = {
+      Draw: {
+        texture(textureUrl, x, y) {
+          const txt = getTexture(textureUrl);
+          Draw.texture(txt, x, y);
+        },
+        stretchedTexture(textureUrl, x, y, width, height, rotation, origin) {
+          const txt = getTexture(textureUrl);
+          Draw.stretchedTexture(txt, x, y, width, height, rotation, origin);
+        },
+      },
+    };
+
+    let state = {
+      screenDimensions: { width: 0, height: 0 },
+    };
+
+    const bundleSourceCode = await loadBundle();
+    const bundleBlob = new Blob([bundleSourceCode]);
+    const scriptUrl = URL.createObjectURL(bundleBlob);
+    gameWebWorker = new Worker(scriptUrl);
+    gameWebWorker.onerror = (e) => {
+      onError(e.message);
+    };
+    gameWebWorker.onmessage = ({ data }) => {
+      const camera = new Camera2D(
+        -state.screenDimensions.width / 2,
+        -state.screenDimensions.height / 2,
+        state.screenDimensions.width,
+        state.screenDimensions.height
+      );
+      Draw.applyCamera(camera);
+
+      renderBegin();
+      clearCanvas([0.2, 0.2, 0.2, 1]);
+      begin();
+
+      for (const drawCall of data) {
+        const [objectName, functionName, ...args] = drawCall;
+        proxyObjects[objectName][functionName](...args);
+      }
+      flush();
+      if (running) {
+        window.requestAnimationFrame(render);
+      }
+    };
+    URL.revokeObjectURL(scriptUrl);
+
+    const render = () => {
+      if (!gameWebWorker) {
         return;
       }
-      initializeRenderer(canvas);
-      let loadedTextures = {};
 
-      const getTexture = (textureUrl) => {
-        if (loadedTextures[textureUrl]) {
-          return loadedTextures[textureUrl];
-        }
-        const textureAbsoluteUrl = getBuiltResourceFile(workspaceId, textureUrl);
-        return (loadedTextures[textureUrl] = Texture.loadLazy(textureAbsoluteUrl));
-      };
+      updateFps();
 
-      const proxyObjects = {
-        Draw: {
-          texture(textureUrl, x, y) {
-            const txt = getTexture(textureUrl);
-            Draw.texture(txt, x, y);
-          },
-          stretchedTexture(textureUrl, x, y, width, height, rotation, origin) {
-            const txt = getTexture(textureUrl);
-            Draw.stretchedTexture(txt, x, y, width, height, rotation, origin);
-          },
-        },
-      };
+      let stateIncr = {};
 
-      let state = {
-        screenDimensions: { width: 0, height: 0 },
-      };
-
-      const bundleSourceCode = await getBuiltJsBundle(workspaceId);
-      const bundleBlob = new Blob([bundleSourceCode]);
-      const scriptUrl = URL.createObjectURL(bundleBlob);
-      gameWebWorkerRef.current = new Worker(scriptUrl);
-      gameWebWorkerRef.current.onerror = e => {
-        showError(e.message);
+      const screenDimensions = GameScreen.getDimensions();
+      if (!deepEqual(state.screenDimensions, screenDimensions)) {
+        state.screenDimensions = screenDimensions;
+        stateIncr = { screenDimensions };
       }
-      gameWebWorkerRef.current.onmessage = ({ data }) => {
-        const camera = new Camera2D(
-          -state.screenDimensions.width / 2,
-          -state.screenDimensions.height / 2,
-          state.screenDimensions.width,
-          state.screenDimensions.height
-        );
-        Draw.applyCamera(camera);
 
-        renderBegin();
-        clearCanvas([0.2, 0.2, 0.2, 1]);
-        begin();
-
-        for (const drawCall of data) {
-          const [objectName, functionName, ...args] = drawCall;
-          proxyObjects[objectName][functionName](...args);
-        }
-        flush();
-        if (running) {
-          window.requestAnimationFrame(render);
-        }
-      };
-      URL.revokeObjectURL(scriptUrl);
-
-      const render = () => {
-        if (!gameWebWorkerRef.current) {
-          return;
-        }
-
-        updateFps();
-
-        let stateIncr = {};
-
-        const screenDimensions = GameScreen.getDimensions();
-        if (!deepEqual(state.screenDimensions, screenDimensions)) {
-          state.screenDimensions = screenDimensions;
-          stateIncr = { screenDimensions };
-        }
-
-        gameWebWorkerRef.current.postMessage({ type: "draw", stateIncr });
-      };
-      window.requestAnimationFrame(render);
+      gameWebWorker.postMessage({ type: "draw", stateIncr });
     };
-    fetchGame();
+    window.requestAnimationFrame(render);
+  };
+  fetchGame();
 
-    return () => {
-      gameWebWorkerRef.current?.terminate();
+  return {
+    cleanup: () => {
+      gameWebWorker?.terminate();
       running = false;
       unitializeRenderer();
-    };
+    },
+  };
 }
